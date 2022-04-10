5fc7064a14a3a1ddd9cc1e4556c590a6b2b5225b
Pexet <pexet.pc@gmail.com>
2022-04-10 18:58:47 +0300

    make serilog and seq

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -2,47 +2,73 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Models;
 using Npgsql;
+using Serilog;
+using Serilog.Events;
 using Services.Implementations;
 using Services.Interfaces;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IBaseEntityService, BaseEntityService>();
-builder.Services.AddScoped<IRepository<BaseEntity>, BaseRepository<BaseEntity>>();
+Log.Information("Starting up");
+Log.Information("Glory to Ukraine!");
 
-// Database context
-var connectionString = GetHerokuConString();
-
-if (connectionString is null)
+try
 {
-    builder.Configuration.AddJsonFile("appsettings.Local.json");
-    connectionString = builder.Configuration.GetConnectionString("DATABASE_URL");
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Seq("http://localhost:5341")
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddScoped<IBaseEntityService, BaseEntityService>();
+    builder.Services.AddScoped<IRepository<BaseEntity>, BaseRepository<BaseEntity>>();
+
+    // Database context
+    var connectionString = GetHerokuConString();
+
+    if (connectionString is null)
+    {
+        builder.Configuration.AddJsonFile("appsettings.Local.json");
+        connectionString = builder.Configuration.GetConnectionString("DATABASE_URL");
+    }
+
+    builder.Services.AddDbContext<ApplicationContext>(x => x.UseNpgsql(connectionString));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsProduction())
+    {
+        app.UseExceptionHandler("/Home/Error");
+
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
 }
 
-builder.Services.AddDbContext<ApplicationContext>(x => x.UseNpgsql(connectionString));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsProduction())
+finally
 {
-    app.UseExceptionHandler("/Home/Error");
-
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
 
 static string? GetHerokuConString()
 {
