0fdafeb23deef7d1e94c14926e9f403ef73759a5
Oleksii Zarechanskyi <misterz06022002@gmail.com>
2022-04-17 23:13:54 +0300

    small change

--- src/Models/Entities/Movie.cs
+++ src/Models/Entities/Movie.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Models.Entities
 {
+    [Table("Movies")]
     public class Movie : BaseEntity
     {
         [Required]
