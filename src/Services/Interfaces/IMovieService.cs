1b9e3acd4aa0aa86cb091881297c1cd8284b5542
Oleksii Zarechanskyi <misterz06022002@gmail.com>
2022-05-04 20:53:20 +0300

    edit movie - edit movie backend and frontend - edit movie unit tests - renamed namespace for unit tests

--- src/Services/Interfaces/IMovieService.cs
+++ src/Services/Interfaces/IMovieService.cs
@@ -6,5 +6,6 @@ namespace Services.Interfaces
     {
         public Task<bool> AddMovieAsync(Movie movie);
         public Task<Movie?> GetMovieAsync(Guid movieId);
+        public Task<bool> EditMovieAsync(Movie tracked, Movie untracked);
     }
 }
