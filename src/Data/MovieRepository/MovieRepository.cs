64871060fe41d5eff715c8b654e71a538e0c1e01
SerhiyToroniy <70684209+SerhiyToroniy@users.noreply.github.com>
2022-05-06 23:07:57 +0300

    Added Home Page functionality 2.

--- src/Data/MovieRepository/MovieRepository.cs
+++ src/Data/MovieRepository/MovieRepository.cs
@@ -10,6 +10,15 @@ namespace Data.MovieRepository
         {
         }
 
+        public async Task<List<Movie>> GetMoviesPart(int page, int pageSize)
+        {
+            // for release
+            // return await _set.OrderByDescending(r => r.Rating.Average(p => p.Rate)).ThenBy(n => n.Name).Skip(page * pageSize).Take(pageSize).ToListAsync();
+
+            // for testing
+            return await _set.OrderByDescending(r => r.Name).Skip(page * pageSize).Take(pageSize).ToListAsync();
+        }
+
         public async Task<Movie?> GetWithRelationsAsync(Guid id)
         {
             var movie = await _set
---
+++ src/Services/Interfaces/IHomeService.cs
@@ -0,0 +1,9 @@
+﻿using Models.Entities;
+
+namespace Services.Interfaces
+{
+    public interface IHomeService
+    {
+        public Task<List<Movie>> GetMoviesPartAsync(int page, int pageSize);
+    }
+}
